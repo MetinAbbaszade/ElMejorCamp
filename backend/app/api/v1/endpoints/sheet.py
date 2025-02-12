55e7f52da5fe6f155b2e097d88d2fde3552bd7e9
Metin100 <9455@holbertonstudents.com>
2025-02-12 15:44:56 +0400

    add

--- backend/app/api/v1/endpoints/sheet.py
+++ backend/app/api/v1/endpoints/sheet.py
@@ -17,50 +17,50 @@ SAMPLE_SPREADSHEET_ID = "183dZIelF1f0iFySm4QfCRYJyCKkUkP7kNWIMhJgWtAo"
 
 @router.post('/', response_model=None, status_code=status.HTTP_204_NO_CONTENT)
 async def add_to_sheet(Model: SheetModel):
-    creds = None
-    token_path = "token.json"
-    credentials_path = "/Users/methiinn/Desktop/elmejor/backend/app/api/v1/schemas/credentials.json"
+    # creds = None
+    # token_path = "token.json"
+    # credentials_path = "/Users/methiinn/Desktop/elmejor/backend/app/api/v1/schemas/credentials.json"
 
-    if os.path.exists(token_path):
-        creds = Credentials.from_authorized_user_file(token_path, SCOPES)
+    # if os.path.exists(token_path):
+    #     creds = Credentials.from_authorized_user_file(token_path, SCOPES)
 
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(credentials_path, SCOPES)
-            creds = flow.run_local_server(port=3000, prompt="consent")
+    # if not creds or not creds.valid:
+    #     if creds and creds.expired and creds.refresh_token:
+    #         creds.refresh(Request())
+    #     else:
+    #         flow = InstalledAppFlow.from_client_secrets_file(credentials_path, SCOPES)
+    #         creds = flow.run_local_server(port=3000, prompt="consent")
 
-        with open(token_path, "w") as token:
-            token.write(creds.to_json())
+    #     with open(token_path, "w") as token:
+    #         token.write(creds.to_json())
 
-    try:
-        service = build("sheets", "v4", credentials=creds)
-        sheet = service.spreadsheets()
+    # try:
+    #     service = build("sheets", "v4", credentials=creds)
+    #     sheet = service.spreadsheets()
 
-        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range="Sheet1!A:A").execute()
-        values = result.get("values", [])
-        next_row = len(values) + 1 
+    #     result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range="Sheet1!A:A").execute()
+    #     values = result.get("values", [])
+    #     next_row = len(values) + 1 
 
-        SAMPLE_RANGE_NAME = f"Sheet1!A{next_row}"
-        id = uuid4()
-        create_time = datetime.now()
-        update_time = datetime.now()
-        Model.id = str(id)
-        Model.created_at = str(create_time)
-        Model.updated_at = str(update_time)
-        valueData = [[Model.parent_info, Model.parent_number, Model.player_info, Model.player_birth_date, \
-            Model.player_adress, Model.player_club, Model.player_position, Model.player_strong_foot, Model.payment, Model.id, \
-                Model.created_at, Model.updated_at]]
-        result = (
-            sheet.values()
-            .update(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME, valueInputOption="USER_ENTERED", body={"values": valueData})
-            .execute()
-        )
+    #     SAMPLE_RANGE_NAME = f"Sheet1!A{next_row}"
+    #     id = uuid4()
+    #     create_time = datetime.now()
+    #     update_time = datetime.now()
+    #     Model.id = str(id)
+    #     Model.created_at = str(create_time)
+    #     Model.updated_at = str(update_time)
+    #     valueData = [[Model.parent_info, Model.parent_number, Model.player_info, Model.player_birth_date, \
+    #         Model.player_adress, Model.player_club, Model.player_position, Model.player_strong_foot, Model.payment, Model.id, \
+    #             Model.created_at, Model.updated_at]]
+    #     result = (
+    #         sheet.values()
+    #         .update(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME, valueInputOption="USER_ENTERED", body={"values": valueData})
+    #         .execute()
+    #     )
 
-    except HttpError as err:
-        print(f"Google Sheets API error: {err}")
-
+    # except HttpError as err:
+    #     print(f"Google Sheets API error: {err}")
+    return 'Salam'
 
 @router.put('/update-payment', response_model=None, status_code=status.HTTP_204_NO_CONTENT)
 async def update_payment(model: updateSheetModel):
