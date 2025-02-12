ba36df6bae116da7aba11349b72f7b591bf69a1e
Metin100 <9455@holbertonstudents.com>
2025-02-12 15:35:07 +0400

    add

--- frontend/script.js
+++ frontend/script.js
@@ -154,7 +154,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const player_strong_foot = document.getElementById('foot').value;
 
         try {
-            const response = await fetch("http://0.0.0.0:8000/api/v1/sheet/", {
+            const response = await fetch("https://elmejorcamp-6.onrender.com/api/v1/sheets/", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
